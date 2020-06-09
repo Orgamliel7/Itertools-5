651c46d5153fe4ca35421e4fd6efe0fb9728534a
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2020-06-09 15:10:32 +0300

    Add files via upload

---
+++ range.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+
+namespace itertools{
+class range {
+public:
+    int start_ind, end_ind;
+
+public:
+    range(int from, int to) : start_ind(from), end_ind(to) {};
+
+    class iterator {
+    public:
+        iterator(int curr = 0) : curr(curr) {};
+    private:
+        int curr;
+
+        iterator operator++() // ++i
+        {
+            curr = curr + 1;
+            return *this; // return curr - equivalent?
+        };
+
+        iterator operator++(int) // i++
+        {
+            iterator temp = curr;
+            curr = curr + 1;
+            return temp;
+        };
+
+        bool operator!=(iterator &other) const { return curr != other.curr; };
+
+        bool operator==(const iterator &it) const { return curr == it.curr; };
+
+        int operator*() { return curr; };
+
+
+        iterator begin() { return iterator(start_ind); };
+
+        iterator end() { return iterator(end_ind); };
+    };
+};
+
+
